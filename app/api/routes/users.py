94d4390abb190cfdd77700444b368f9d2d7c4ce8
Giovanni Bronca <giovanni.bronca@artlogic.net>
2024-09-05 16:32:58 +0100

    users endpoits

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from ...core.monday import monday_client
 
@@ -6,5 +6,19 @@ router = APIRouter()
 
 
 @router.get("/")
-async def read_users():
-    return await monday_client.users.fetch_users()
+async def read_users(
+    ids: str | list[str] | None = Query(None),
+    emails: str | list[str] | None = Query(None),
+    kind: str | None = None,
+    limit: int | None = None,
+    name: str | None = None,
+    page: int = 1,
+) -> dict:
+    return await monday_client.users.fetch_users(
+        ids=ids, emails=emails, kind=kind, limit=limit, name=name, page=page
+    )
+
+
+@router.get("/{user_id}")
+async def read_user(user_id: str) -> dict:
+    return await monday_client.users.fetch_users(ids=user_id)
